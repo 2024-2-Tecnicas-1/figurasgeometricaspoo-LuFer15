bda5b59e063ad527b2d35102d5036e15f8801b9b
LuFer15 <luisafernandap70@gmail.com>
2024-10-20 19:55:06 -0500

    Se adicionan clases con métodos y atributos, junto a su complejidad

--- src/main/java/com/mycompany/figurasgeometricas/ClaseRectangulo.java
+++ src/main/java/com/mycompany/figurasgeometricas/ClaseRectangulo.java
@@ -1,5 +1,34 @@
 package com.mycompany.figurasgeometricas;
 
-public class ClaseRectangulo {
+public class ClaseRectangulo extends ClaseFiguraGeometrica {
+    private double lado1;
+    private double lado2;
 
+    public ClaseRectangulo(String nombre, String color, double lado1, double lado2 ){
+        super(nombre, color);
+        this.lado1=lado1;
+        this.lado2=lado2;
+    }
+
+    public double getLado1(){ 
+        return lado1;
+    }
+    public void setLado1(double lado1){ 
+        this.lado1=lado1;
+    }
+    public double getLado2(){ 
+        return lado2;
+    }
+    public void setLado2(double lado2){ 
+        this.lado2=lado2;
+    }
+    @Override
+    public double obtenerArea(){
+        return lado1*lado2;
+    }
+    
+    @Override
+    public double obtenerPerimetro(){
+        return 2 * (lado1+lado2);
+    }
 }
--- src/main/java/com/mycompany/figurasgeometricas/ClaseTriangulo.java
+++ src/main/java/com/mycompany/figurasgeometricas/ClaseTriangulo.java
@@ -1,5 +1,39 @@
 package com.mycompany.figurasgeometricas;
 
-public class ClaseTriangulo {
 
+public class ClaseTriangulo extends ClaseFiguraGeometrica {
+    private double base;
+    private double altura;
+
+    public ClaseTriangulo(String nombre, String color, double base, double altura){
+        super(nombre, color);
+        this.base=base;
+        this.altura=altura;
+    }
+
+    public double getBase(){
+        return base;
+    }
+    public void setBase(double base){
+        this.base=base;
+    }
+    public double getAltura(){
+        return altura;
+    }
+    public void setAltura(double altura){
+        this.altura=altura;
+    }
+
+    @Override
+    public double obtenerArea(){
+        return (base*altura)/2;
+    }
+    
+    @Override
+    public double obtenerPerimetro(){
+        double hipotenusa = Math.sqrt(base*base + altura*altura);
+        return base+altura+hipotenusa;
+    }
 }
+
+
